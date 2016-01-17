fc642771f56446d8f6ac320ffc2cb9670db21c0a
fnwiya <fnwiya@gmail.com>
2016-01-17 14:41:45 +0900

    update

--- abc/abc007/d.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-a, b = map(int, input().split())
-four_cnt = 0
-for i in range(a, b + 1):
-    s = str(i)
-    if s.find("4") or s.find("9"):
-        four_cnt += 1
-print(four_cnt)
