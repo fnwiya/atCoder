8428049d171fb253176f40e97231e4e08296911b
fnwiya <fnwiya@gmail.com>
2016-01-24 01:22:50 +0900

    update

--- abc/abc009/qr_19719zAf.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-n = int(input())
-a = [int(input()) for _ in range(n)]
-a = [3, 4, 5]
-ret = list(set(a))
-ret = ret.sort()
-ret = ret.reverse()
-print(ret[1])
