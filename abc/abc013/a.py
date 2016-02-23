fcd7afa3efb52cedbb778d42b976b4244616754a
fnwiya <fnwiya@gmail.com>
2016-02-23 14:50:46 +0900

    update

---
+++ abc/abc013/a.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+x = str(input())
+alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print(alphabet.index(x)+1)
---
+++ abc/abc013/b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+a = int(input())
+b = int(input())
+if abs(a-b) < 5:
+    print(abs(a-b))
+else:
+    print(10-abs(a-b))
