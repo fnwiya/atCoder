f57de267236c354abd97369c4cbca61720964285
fnwiya <fnwiya@gmail.com>
2016-05-04 16:24:05 +0900

    update

--- abc/abc006/b.js
+++ abc/abc006/b.js
@@ -1,9 +1,14 @@
-function Main(n) {
-    if (parseInt(n, 10) % 3 === 0) {
-        console.log("YES");
-    } else{
-        console.log("NO");
+function Main(input) {
+    var n = parseInt(input, 10);
+    console.log(getTribonach(n));
+}
+
+function getTribonach(n) {
+    var triboAry = [0, 0, 1];
+    for(var i = 3; i < n; i++) {
+        triboAry[i] = (triboAry[i - 1] + triboAry[i - 2] + triboAry[i - 3])  % 10007;
     }
+    return triboAry[n-1];
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
