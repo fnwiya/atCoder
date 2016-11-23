ac1397c059b531f3e15faecc03478091dffa331d
fumiya_kohno <fumiya_kohno@forcia.com>
2016-11-23 23:59:19 +0900

    update

---
+++ abc/abc017/b.hs
@@ -0,0 +1,15 @@
+import Control.Applicative
+
+isChoku :: String -> Bool
+isChoku [] = True
+isChoku (x:xs)
+    | (x == 'o' || x == 'k' || x == 'u') = isChoku xs
+    | (x == 'h' && (head xs) == 'c') = isChoku $ tail xs
+isChoku _ = False
+
+showYesNo :: Bool -> String
+showYesNo b = if b then "YES" else "NO"
+
+main :: IO ()
+main = do
+    main = getLine >>= putStrLn . showYesNo . isChoku
