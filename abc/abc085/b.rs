762dd803cefae56981a7b8581aeebbe2d8e727cf
fnwiya <fnwiya@gmail.com>
2018-08-12 15:06:53 +0900

    Solve abc085b

---
+++ abc/abc085/b.rs
@@ -0,0 +1,21 @@
+use std::io::*;
+use std::str::FromStr;
+use std::collections::BTreeSet;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn main() {
+    let n: u32 = read();
+    let mut a: BTreeSet<u32> = (0..n).map(|_| read()).collect();
+    println!("{}", a.len());
+}
