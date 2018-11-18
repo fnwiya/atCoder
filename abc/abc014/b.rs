bff64e55b20984eb05093c9c4b4288d26194dec9
fnwiya <fnwiya@gmail.com>
2018-11-18 21:27:07 +0900

    Solve abc014_b

---
+++ abc/abc014/b.rs
@@ -0,0 +1,26 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn main() {
+    let n: usize = read();
+    let mut x: usize = read();
+    let mut ans = 0;
+    for i in 0..n {
+        let a: usize = read();
+        ans += x % 2 * a;
+        x /= 2;
+    }
+    println!("{}", ans)
+}
