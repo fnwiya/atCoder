325048f6283e7435f3679843ec67ec6267f71b7f
fnwiya <fnwiya@gmail.com>
2023-09-12 22:59:39 +0900

    solve

---
+++ abc/abc015/c.rs
@@ -0,0 +1,24 @@
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+    k: usize,
+    t: [[usize; k]; n]
+  }
+  let found = dfs(0, 0, n, k, &t);
+  println!("{}", if found { "Found" } else { "Nothing" });
+}
+
+fn dfs(
+  num_q: usize,
+  value: usize,
+  n: usize,
+  k: usize,
+  t: &Vec<Vec<usize>>,
+) -> bool {
+  if num_q == n {
+    return value == 0
+  }
+  return (0..k).any(|i| dfs(num_q + 1, value ^ t[num_q][i], n, k, &t));
+}
